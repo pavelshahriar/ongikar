b0b97434c6c2c1ab19b910da107c93b58cd9544d
Pavel Shahriar <shahriar.tanvir@gmail.com>
2016-04-04 04:17:42 -0400

    Project setup done. Node, Express, Gulp, Sass, Handlebars - all setup complete.

---
+++ app.js
@@ -0,0 +1,52 @@
+// set variables for environment
+var express = require('express');
+var path = require('path');
+var exphbs = require('express-handlebars');
+
+var routes = require('./routes/index');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+
+// instruct express to server up static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set routes
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req,res,next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
