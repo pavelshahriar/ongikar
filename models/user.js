4dba1712de56d4c331d6a1e38b74747f4eaa3ebf
Pavel Shahriar <shahriar.tanvir@gmail.com>
2016-04-11 04:57:13 -0400

    Integrated MongoDB. Integrated routers and functionality for /api/users/* endpoints

---
+++ models/user.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created_at: Date,
+    updated_at: Date
+});
+
+// on every save, add the date
+userSchema.pre('save', function(next) {
+    // get the current date
+    var currentDate = new Date();
+
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at)
+        this.created_at = currentDate;
+
+    next();
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
