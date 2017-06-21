c7891e9951fc30257ac79bfea988ef9140c6aee9
Pavel Shahriar <shahriar.tanvir@gmail.com>
2017-06-21 17:53:35 -0400

    fix build

--- gulp/tasks/newman.js
+++ gulp/tasks/newman.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('dotenv').config();
+require('dotenv').config({path: '.env'});
 
 const gulp = require('gulp');
 const exec = require('child_process').exec;
@@ -10,7 +10,7 @@ const config = require('../config');
 
 // Run newman tests
 gulp.task('newman', ['apidoc'], (cb) => {
-    exec('newman run -c ' + config.newman.path +'*.collection ' + '-e ' + config.newman.path + process.env.NODE_ENV + '.environment',
+    exec('newman run ' + config.newman.path +'*.collection ' + '-e ' + config.newman.path + process.env.NODE_ENV + '.environment',
         (err, stdout, stderr) => {
             console.log(stdout);
             console.log(stderr);
